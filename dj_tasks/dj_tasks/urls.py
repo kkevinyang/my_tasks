4dda859922870fc519ad8d141306a56cc96039f4
杨凯 <yangkai@192.168.1.6>
2018-02-06 23:47:39 +0800

    init

---
+++ dj_tasks/dj_tasks/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'dj_tasks.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
+    url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
+    url(r'^admin/', include(admin.site.urls)),
+]
